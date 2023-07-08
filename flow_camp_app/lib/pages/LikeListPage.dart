bf2b0ec81a1b35b7742d744ece1d9c0feceb1678
Sebyeol23 <lixxil2323@gmail.com>
2023-07-08 20:11:17 +0900

    Design(LikeListPage): 기본 UI

--- flow_camp_app/lib/pages/LikeListPage.dart
+++ flow_camp_app/lib/pages/LikeListPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class LikeListPage extends StatefulWidget {
   const LikeListPage({Key? key}) : super(key: key);
@@ -8,8 +9,65 @@ class LikeListPage extends StatefulWidget {
 }
 
 class _LikeListPageState extends State<LikeListPage> {
+  final List<String> entries = <String>[
+    "A",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C",
+    "B",
+    "C"
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: CupertinoSearchTextField(),
+      ),
+      child: SafeArea(
+        child: ListView.builder(
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: CupertinoListTile(
+                  leadingSize: 50,
+                  title: Text("${entries[index]}",
+                    style: TextStyle(decoration: TextDecoration.none)
+                  ),
+                  // subtitle: Text("${entries[index]}",
+                  //   style: TextStyle(decoration: TextDecoration.none)
+                  // ),
+                  leading: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
