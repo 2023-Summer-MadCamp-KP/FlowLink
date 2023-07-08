99697b0047d9c2d88774e17914c4113910808678
Sebyeol23 <lixxil2323@gmail.com>
2023-07-08 21:02:52 +0900

    Design(LikeListPage): Person Class 생성

--- flow_camp_app/lib/pages/LikeListPage.dart
+++ flow_camp_app/lib/pages/LikeListPage.dart
@@ -8,28 +8,17 @@ class LikeListPage extends StatefulWidget {
   State<LikeListPage> createState() => _LikeListPageState();
 }
 
+class Person {
+  late String name;
+  late Image profileImage;
+
+  Person(this.name, Image? profileImage) {
+    this.profileImage = profileImage ?? Image.asset("images/googleIcon.png");
+  }
+}
+
 class _LikeListPageState extends State<LikeListPage> {
-  final List<String> entries = <String>[
-    "A",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C",
-    "B",
-    "C"
-  ];
+  final List<Person> people = <Person>[Person("아무개", null), Person("나나나", Image.asset("images/flowlinkLogo.png")), Person("시나모롤", Image.asset("images/roll.jpg"))];
   @override
   void initState() {
     // TODO: implement initState
@@ -39,33 +28,27 @@ class _LikeListPageState extends State<LikeListPage> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+      navigationBar: const CupertinoNavigationBar(
         middle: CupertinoSearchTextField(),
       ),
       child: SafeArea(
         child: ListView.builder(
-            itemCount: entries.length,
+            itemCount: people.length,
             itemBuilder: (BuildContext context, int index) {
               return Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: CupertinoListTile(
-                  leadingSize: 50,
-                  title: Text("${entries[index]}",
-                    style: TextStyle(decoration: TextDecoration.none)
-                  ),
-                  // subtitle: Text("${entries[index]}",
-                  //   style: TextStyle(decoration: TextDecoration.none)
-                  // ),
-                  leading: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: CupertinoListTile(
+                    leadingSize: 40,
+                    title: Text(people[index].name,
+                        style: const TextStyle(decoration: TextDecoration.none)),
+                    // subtitle: Text("${entries[index]}",
+                    //   style: TextStyle(decoration: TextDecoration.none)
+                    // ),
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: people[index].profileImage,
                     ),
-                  ),
-                ),
-              );
+                  ));
             }),
       ),
     );
