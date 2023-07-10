4addcd63c783cfe7575948ea75501ff329662431
skykhs3 <skykhs3@naver.com>
2023-07-10 19:47:33 +0900

    Style(sign_up_page):코드 정렬

--- flow_camp_app/lib/pages/sign_up_page.dart
+++ flow_camp_app/lib/pages/sign_up_page.dart
@@ -24,17 +24,6 @@ class _SignUpPageState extends State<SignUpPage> {
   bool _isObscure = true;
   late FocusNode _idFocusNode, _pwFocusNode, _pwConfirmFocusNode;
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     // This call to setState tells the Flutter framework that something has
-  //     // changed in this State, which causes it to rerun the build method below
-  //     // so that the display can reflect the updated values. If we changed
-  //     // _counter without calling setState(), then the build method would not be
-  //     // called again, and so nothing would appear to happen.
-  //     _counter++;
-  //   });
-  // }
-
   @override
   void initState() {
     super.initState();
@@ -59,175 +48,182 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    Future<void> _signUp () async{
+    Future<void> _signUp() async {
       if (_pwController.text != _pwConfirmController.text) {
-          showDialog(
-            context: context,
-            builder: (_) => CupertinoAlertDialog(
-              title: Text('Error'),
-              content: Text('The passwords do not match. Please try again.'),
-              actions: <Widget>[
-                CupertinoDialogAction(
-                  isDefaultAction: true,
-                  child: Text('Ok'),
-                  onPressed: () {
-                    Navigator.of(context, rootNavigator: true).pop('dialog');
-                  },
-                ),
-              ],
-            ),
-          );
+        showDialog(
+          context: context,
+          builder: (_) => CupertinoAlertDialog(
+            title: Text('Error'),
+            content: Text('The passwords do not match. Please try again.'),
+            actions: <Widget>[
+              CupertinoDialogAction(
+                isDefaultAction: true,
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context, rootNavigator: true).pop('dialog');
+                },
+              ),
+            ],
+          ),
+        );
+      } else {
+        setState(() {
+          _isLoading = true;
+        });
+
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+          });
         }
+      }
       return;
     }
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
-          leading: CupertinoNavigationBarBackButton(
-
-          ),
+          leading: CupertinoNavigationBarBackButton(),
         ),
-        child: SafeArea(
-          child: Scaffold(
-            backgroundColor: Colors.white,
-            body: SizedBox(
-                    width: double.infinity,
-                    height: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            SizedBox(
-                              width: 200,
-                              height: 50,
-                              child: Image.asset("assets/images/flowlinkLogo.png"),
-                            ),
-                            const SizedBox(
-                              width: double.infinity,
-                              height: 30,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              height: 40,
-                              child: CupertinoTextField(
-                                controller: _idController,
-                                focusNode: _idFocusNode,
-                                textInputAction: TextInputAction.next,
-                                padding: const EdgeInsets.only(left: 10),
-                                textAlignVertical: TextAlignVertical.center,
-                                placeholder: "Username",
-                                placeholderStyle: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w500,
-                                    height: 1.5,
-                                    color: Colors.grey),
-                                decoration: BoxDecoration(
-                                    color: const Color.fromARGB(
-                                        255, 248, 248, 248),
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(
-                                      width: 1,
-                                      color: const Color.fromARGB(
-                                          255, 220, 220, 220),
-                                    )),
+        child: _isLoading
+            ? CupertinoActivityIndicator()
+            : SafeArea(
+                child: Scaffold(
+                  backgroundColor: Colors.white,
+                  body: SizedBox(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(
+                                width: 200,
+                                height: 50,
+                                child: Image.asset(
+                                    "assets/images/flowlinkLogo.png"),
                               ),
-                            ),
-                            const SizedBox(
-                              width: double.infinity,
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              height: 40,
-                              child: CupertinoTextField(
-                                controller: _pwController,
-                                obscureText: _isObscure,
-                                focusNode: _pwFocusNode,
-                                textInputAction: TextInputAction.go,
-                                padding: const EdgeInsets.only(left: 10),
-                                textAlignVertical: TextAlignVertical.center,
-                                placeholder: "Password",
-                                placeholderStyle: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w500,
-                                    height: 1.5,
-                                    color: Colors.grey),
-                                decoration: BoxDecoration(
-                                    color: const Color.fromARGB(
-                                        255, 248, 248, 248),
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(
-                                      width: 1,
+                              const SizedBox(
+                                width: double.infinity,
+                                height: 30,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: CupertinoTextField(
+                                  controller: _idController,
+                                  focusNode: _idFocusNode,
+                                  textInputAction: TextInputAction.next,
+                                  padding: const EdgeInsets.only(left: 10),
+                                  textAlignVertical: TextAlignVertical.center,
+                                  placeholder: "Username",
+                                  placeholderStyle: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.5,
+                                      color: Colors.grey),
+                                  decoration: BoxDecoration(
                                       color: const Color.fromARGB(
-                                          255, 220, 220, 220),
-                                    )),
-                                suffix: SizedBox(
-                                  width: 40,
-                                  height: double.infinity,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        _isObscure = !_isObscure;
-                                      });
-                                    },
-                                    icon: Icon(
-                                        _isObscure
-                                            ? Icons.visibility
-                                            : Icons.visibility_off,
-                                        color: Colors.grey),
+                                          255, 248, 248, 248),
+                                      borderRadius: BorderRadius.circular(5),
+                                      border: Border.all(
+                                        width: 1,
+                                        color: const Color.fromARGB(
+                                            255, 220, 220, 220),
+                                      )),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: double.infinity,
+                                height: 10,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: CupertinoTextField(
+                                  controller: _pwController,
+                                  obscureText: _isObscure,
+                                  focusNode: _pwFocusNode,
+                                  textInputAction: TextInputAction.go,
+                                  padding: const EdgeInsets.only(left: 10),
+                                  textAlignVertical: TextAlignVertical.center,
+                                  placeholder: "Password",
+                                  placeholderStyle: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.5,
+                                      color: Colors.grey),
+                                  decoration: BoxDecoration(
+                                      color: const Color.fromARGB(
+                                          255, 248, 248, 248),
+                                      borderRadius: BorderRadius.circular(5),
+                                      border: Border.all(
+                                        width: 1,
+                                        color: const Color.fromARGB(
+                                            255, 220, 220, 220),
+                                      )),
+                                  suffix: SizedBox(
+                                    width: 40,
+                                    height: double.infinity,
+                                    child: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _isObscure = !_isObscure;
+                                        });
+                                      },
+                                      icon: Icon(
+                                          _isObscure
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
+                                          color: Colors.grey),
+                                    ),
                                   ),
-                                ),       
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: double.infinity,
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              height: 40,
-                              child: CupertinoTextField(
-                                controller: _pwConfirmController,
-                                obscureText: true,
-                                focusNode: _pwConfirmFocusNode,
-                                textInputAction: TextInputAction.go,
-                                padding: const EdgeInsets.only(left: 10),
-                                textAlignVertical: TextAlignVertical.center,
-                                placeholder: "Confirm Password",
-                                placeholderStyle: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w500,
-                                    height: 1.5,
-                                    color: Colors.grey),
-                                decoration: BoxDecoration(
-                                    color: const Color.fromARGB(
-                                        255, 248, 248, 248),
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(
-                                      width: 1,
+                              const SizedBox(
+                                width: double.infinity,
+                                height: 10,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: CupertinoTextField(
+                                  controller: _pwConfirmController,
+                                  obscureText: true,
+                                  focusNode: _pwConfirmFocusNode,
+                                  textInputAction: TextInputAction.go,
+                                  padding: const EdgeInsets.only(left: 10),
+                                  textAlignVertical: TextAlignVertical.center,
+                                  placeholder: "Confirm Password",
+                                  placeholderStyle: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.5,
+                                      color: Colors.grey),
+                                  decoration: BoxDecoration(
                                       color: const Color.fromARGB(
-                                          255, 220, 220, 220),
-                                    )),     
+                                          255, 248, 248, 248),
+                                      borderRadius: BorderRadius.circular(5),
+                                      border: Border.all(
+                                        width: 1,
+                                        color: const Color.fromARGB(
+                                            255, 220, 220, 220),
+                                      )),
+                                ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: double.infinity,
-                              height: 50,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              height: 40,
-                              child: CupertinoButton.filled(
+                              const SizedBox(
+                                width: double.infinity,
+                                height: 50,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: CupertinoButton.filled(
                                   padding: const EdgeInsets.all(0),
                                   child: const Text(
                                     "Sign Up",
@@ -236,14 +232,14 @@ class _SignUpPageState extends State<SignUpPage> {
                                         fontWeight: FontWeight.bold),
                                   ),
                                   onPressed: _signUp,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    )),
-          ),
-        ),
+                      )),
+                ),
+              ),
       ),
     );
   }
