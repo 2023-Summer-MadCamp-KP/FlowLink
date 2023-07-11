2e2d2ab0ea50b9c7f8faf412d9af5e7803afb3f9
skykhs3 <skykhs3@naver.com>
2023-07-11 14:38:09 +0900

    Update test.js

--- flow_camp_server/routes/test.js
+++ flow_camp_server/routes/test.js
@@ -6,26 +6,41 @@ const { Like } = require('../models');
 
 router.get('/adddummyuser', async function (req, res, next) {
   for (let i = 0; i < 100; i++) {
-    let key=i.toString();
-    await User.create({
-      
-      uid: key,
-      password: key+key,
-      platform: "normal",
-      infoConfirmed: true,
-      name: 'user'+i,
-      prtcpntYear:20232,
-      gradOf:20190204,
-      infoConfirmed: true,
-    })
+    let key = i.toString();
+    if (i < 10) {
+      await User.create({
+
+        uid: key,
+        password: key + key,
+        platform: "normal",
+        infoConfirmed: true,
+        name: '유저' + i,
+        prtcpntYear: 202324,
+        gradOf: 20190204,
+        infoConfirmed: true,
+      })
+    }
+    else {
+      await User.create({
+
+        uid: key,
+        password: key + key,
+        platform: "normal",
+        infoConfirmed: true,
+        name: '유저' + i,
+        prtcpntYear: 202321,
+        gradOf: 20190204,
+        infoConfirmed: true,
+      })
+    }
   }
 
   res.send();
 });
 router.get('/addlike', async function (req, res, next) {
-  const user = await User.findOne({ where: { id: 1 }});
+  const user = await User.findOne({ where: { id: 1 } });
   const user2 = await User.findOne({ where: { id: 2 } });
-  
+
   const [like, created] = await Like.findOrCreate({
     where: { likeFrom: user.id, likeTo: user2.id }
   });
@@ -35,35 +50,35 @@ router.get('/addlike', async function (req, res, next) {
   } else {
     res.json({ message: "Like already exists" });
   }
-// try {
-//   const [user, created] = await User.findOrCreate({
-//     where: { username: 'sdepold' },
-//     defaults: { job: 'Technical Lead JavaScript' },
-//     transaction
-//   });
+  // try {
+  //   const [user, created] = await User.findOrCreate({
+  //     where: { username: 'sdepold' },
+  //     defaults: { job: 'Technical Lead JavaScript' },
+  //     transaction
+  //   });
 
-//   // If the execution reaches this line, the transaction has been successfully committed
-//   await transaction.commit();
-// } catch (error) {
-//   // If the execution reaches this line, an error occurred.
-//   // The transaction has been rolled back automatically
-//   await transaction.rollback();
-// }
+  //   // If the execution reaches this line, the transaction has been successfully committed
+  //   await transaction.commit();
+  // } catch (error) {
+  //   // If the execution reaches this line, an error occurred.
+  //   // The transaction has been rolled back automatically
+  //   await transaction.rollback();
+  // }
 });
 
 
 router.get('/adddummyinterest', async function (req, res, next) {
   for (let i = 0; i < 100; i++) {
-    let key=i.toString();
+    let key = i.toString();
     await User.create({
-      
+
       uid: key,
-      password: key+key,
+      password: key + key,
       platform: "normal",
       infoConfirmed: true,
-      name: 'user'+i,
-      prtcpntYear:20232,
-      gradOf:20190204,
+      name: 'user' + i,
+      prtcpntYear: 20232,
+      gradOf: 20190204,
       infoConfirmed: true,
     })
   }
