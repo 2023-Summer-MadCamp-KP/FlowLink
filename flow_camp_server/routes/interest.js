ee3b93aae5e77984bc7e41fd480f5ead2b670c9e
Sebyeol23 <lixxil2323@gmail.com>
2023-07-11 15:22:18 +0900

    Feat(Interest): Interest List get api

--- flow_camp_server/routes/interest.js
+++ flow_camp_server/routes/interest.js
@@ -5,8 +5,13 @@ const { Interest } = require('../models');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     const interest = await Interest.findAll();
-    console.log(interest[0].name);
-    res.send("hi");
+    var interestList = [];
+    interest.forEach((value, index, array)=>{
+        if(value.confirmed){
+            interestList.push(value.name);
+        }
+    })
+    res.send(interestList);
 });
 
 module.exports = router;
