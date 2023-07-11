c70df78cc83827ee12e4cf6ea1cee0658323f72e
Sebyeol23 <lixxil2323@gmail.com>
2023-07-11 21:37:02 +0900

    Feat(LikeListPage): to, from, both 필터 기능 추가

--- flow_camp_app/lib/pages/like_list_page.dart
+++ flow_camp_app/lib/pages/like_list_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:flow_camp_app/providers/user_provider.dart';
+import 'package:flow_camp_app/models/user.dart';
+import 'package:flow_camp_app/components/loading_indicator_page.dart';
 
 class LikeListPage extends StatefulWidget {
   const LikeListPage({Key? key}) : super(key: key);
@@ -8,16 +12,46 @@ class LikeListPage extends StatefulWidget {
   State<LikeListPage> createState() => _LikeListPageState();
 }
 
-class Person {
-  late String name;
-  late Image profileImage;
-  late bool like;
-  late List<String> categoryList;
+// class Person {
+//   late String name;
+//   late Image profileImage;
+//   late bool like;
+//   late List<String> categoryList;
 
-  Person(this.name, Image? profileImage, this.like, this.categoryList) {
-    this.profileImage =
-        profileImage ?? Image.asset("assets/images/googleIcon.png");
-  }
+//   Person(this.name, Image? profileImage, this.like, this.categoryList) {
+//     this.profileImage =
+//         profileImage ?? Image.asset("assets/images/googleIcon.png");
+//   }
+// }
+
+class Person extends User {
+  var profileImage;
+
+  var islike;
+
+  Person({
+    required int id,
+    required String name,
+    required int gradOf,
+    required String uid,
+    required String password,
+    required String platform,
+    required int prtcpntYear,
+    required bool emailConfirmed,
+    required bool infoConfirmed,
+    required this.profileImage,
+    required this.islike,
+  }) : super(
+          id: id,
+          name: name,
+          gradOf: gradOf,
+          uid: uid,
+          password: password,
+          platform: platform,
+          prtcpntYear: prtcpntYear,
+          emailConfirmed: emailConfirmed,
+          infoConfirmed: infoConfirmed,
+        );
 }
 
 class _LikeListPageState extends State<LikeListPage> {
@@ -29,23 +63,41 @@ class _LikeListPageState extends State<LikeListPage> {
     "VR",
     "Blockchain"
   ];
-  final List<Person> people = <Person>[
-    Person("아무개", null, false, []),
-    Person("나나나", Image.asset("assets/images/flowlinkLogo.png"), false,
-        ["App", "Web"]),
-    Person("시나모롤", Image.asset("assets/images/roll.jpg"), true,
-        ["Web", "AI", "Game"])
-  ];
+  // List<Person> people = <Person>[
+  //   Person("아무개", null, false, []),
+  //   Person("나나나", Image.asset("assets/images/flowlinkLogo.png"), false,
+  //       ["App", "Web"]),
+  //   Person("시나모롤", Image.asset("assets/images/roll.jpg"), true,
+  //       ["Web", "AI", "Game"])
+  // ];
+  List<Person> peopleLikeTo = [];
+  List<Person> peopleLikeFrom = [];
+  List<Person> peopleLikeBoth = [];
 
   late List<Person> peopleShow = [];
 
-  bool _likeFilter = false;
+  int _likeFilter = 0;
+  List<String> _likeFilterText = ["To", "From", "Both"];
   List<String> _categoryFilter = [];
+
+  bool _isLoading = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    api();
+  }
+
+  void api() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var provider = context.read<UserProvider>();
+    await provider.getWhoLike();
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   void _showLike(BuildContext ctx) {
@@ -56,19 +108,27 @@ class _LikeListPageState extends State<LikeListPage> {
                 CupertinoActionSheetAction(
                     onPressed: () {
                       setState(() {
-                        _likeFilter = false;
+                        _likeFilter = 0;
                       });
                       _close(_);
                     },
-                    child: const Text("전체")),
+                    child: const Text("likeTo")),
                 CupertinoActionSheetAction(
                     onPressed: () {
                       setState(() {
-                        _likeFilter = true;
+                        _likeFilter = 1;
                       });
                       _close(_);
                     },
-                    child: const Text("like")),
+                    child: const Text("likeFrom")),
+                CupertinoActionSheetAction(
+                    onPressed: () {
+                      setState(() {
+                        _likeFilter = 2;
+                      });
+                      _close(_);
+                    },
+                    child: const Text("likeBoth")),
               ],
               cancelButton: CupertinoActionSheetAction(
                 onPressed: () => _close(_),
@@ -133,27 +193,199 @@ class _LikeListPageState extends State<LikeListPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_likeFilter) {
-      peopleShow.clear();
-      for (int i = 0; i < people.length; i++) {
-        if (people[i].like) {
-          peopleShow.add(people[i]);
-        }
+    var provider = context.watch<UserProvider>();
+    if (_isLoading || provider.me == null) {
+      return LoadingIndicator();
+    }
+    var my = Person(
+      id: provider.me!.id,
+      name: provider.me!.name,
+      gradOf: provider.me!.gradOf,
+      uid: provider.me!.uid,
+      password: provider.me!.password,
+      platform: provider.me!.platform,
+      prtcpntYear: provider.me!.prtcpntYear,
+      emailConfirmed: provider.me!.emailConfirmed,
+      infoConfirmed: provider.me!.infoConfirmed,
+      profileImage: 'assets/images/default_profile.png',
+      islike: false, // Add your images here
+    );
+    Set<int> likeToValues =
+        provider.takeLikes.map((like) => like.likeFrom).toSet();
+    print("llll : " + likeToValues.toString());
+    Set<int> likeFromValues =
+        provider.giveLikes.map((like) => like.likeTo).toSet();
+    Set<int> likeBothValues = {};
+    for (int element in likeToValues) {
+      if (likeFromValues.contains(element)) {
+        likeBothValues.add(element);
       }
+    }
+    print("llllll : " + likeBothValues.toString());
+
+    peopleLikeTo = provider.users
+        .map(
+          (user) {
+            var ii = true;
+            if (likeToValues.contains(user.id)) {
+              ii = true;
+            } else {
+              ii = false;
+            }
+            if (ii) {
+              return Person(
+                id: user.id,
+                name: user.name,
+                gradOf: user.gradOf,
+                uid: user.uid,
+                password: user.password,
+                platform: user.platform,
+                prtcpntYear: user.prtcpntYear,
+                emailConfirmed: user.emailConfirmed,
+                infoConfirmed: user.infoConfirmed,
+                profileImage: 'assets/images/default_profile.png',
+                islike: ii,
+              );
+            } else {
+              return null; // Nullable한 Person 객체는 null로 반환
+            }
+          },
+        )
+        .whereType<Person>() // null이 아닌 Person 객체들만 필터링
+        .toList();
+    peopleLikeTo.sort((a, b) {
+      if (a.id != my.id && b.id == my.id) {
+        return 1;
+      } else if (a.prtcpntYear == my.prtcpntYear &&
+          b.prtcpntYear != my.prtcpntYear) {
+        return -1;
+      } else if (a.prtcpntYear != my.prtcpntYear &&
+          b.prtcpntYear == my.prtcpntYear) {
+        return 1;
+      } else {
+        return b.prtcpntYear.compareTo(a.prtcpntYear);
+      }
+    });
+
+    peopleLikeFrom = provider.users
+        .map(
+          (user) {
+            var ii = true;
+            if (likeFromValues.contains(user.id)) {
+              ii = true;
+            } else {
+              ii = false;
+            }
+            if (ii) {
+              return Person(
+                id: user.id,
+                name: user.name,
+                gradOf: user.gradOf,
+                uid: user.uid,
+                password: user.password,
+                platform: user.platform,
+                prtcpntYear: user.prtcpntYear,
+                emailConfirmed: user.emailConfirmed,
+                infoConfirmed: user.infoConfirmed,
+                profileImage: 'assets/images/default_profile.png',
+                islike: ii,
+              );
+            } else {
+              return null; // Nullable한 Person 객체는 null로 반환
+            }
+          },
+        )
+        .whereType<Person>() // null이 아닌 Person 객체들만 필터링
+        .toList();
+    peopleLikeFrom.sort((a, b) {
+      if (a.id != my.id && b.id == my.id) {
+        return 1;
+      } else if (a.prtcpntYear == my.prtcpntYear &&
+          b.prtcpntYear != my.prtcpntYear) {
+        return -1;
+      } else if (a.prtcpntYear != my.prtcpntYear &&
+          b.prtcpntYear == my.prtcpntYear) {
+        return 1;
+      } else {
+        return b.prtcpntYear.compareTo(a.prtcpntYear);
+      }
+    });
+
+
+    peopleLikeBoth = provider.users
+        .map(
+          (user) {
+            var ii = true;
+            if (likeBothValues.contains(user.id)) {
+              ii = true;
+            } else {
+              ii = false;
+            }
+            if (ii) {
+              return Person(
+                id: user.id,
+                name: user.name,
+                gradOf: user.gradOf,
+                uid: user.uid,
+                password: user.password,
+                platform: user.platform,
+                prtcpntYear: user.prtcpntYear,
+                emailConfirmed: user.emailConfirmed,
+                infoConfirmed: user.infoConfirmed,
+                profileImage: 'assets/images/default_profile.png',
+                islike: ii,
+              );
+            } else {
+              return null; // Nullable한 Person 객체는 null로 반환
+            }
+          },
+        )
+        .whereType<Person>() // null이 아닌 Person 객체들만 필터링
+        .toList();
+    peopleLikeBoth.sort((a, b) {
+      if (a.id != my.id && b.id == my.id) {
+        return 1;
+      } else if (a.prtcpntYear == my.prtcpntYear &&
+          b.prtcpntYear != my.prtcpntYear) {
+        return -1;
+      } else if (a.prtcpntYear != my.prtcpntYear &&
+          b.prtcpntYear == my.prtcpntYear) {
+        return 1;
+      } else {
+        return b.prtcpntYear.compareTo(a.prtcpntYear);
+      }
+    });
+
+    if (_likeFilter == 0) {
+      peopleShow = peopleLikeTo;
+    } else if (_likeFilter == 1) {
+      peopleShow = peopleLikeFrom;
     } else {
-      peopleShow = List.from(people);
+      peopleShow = peopleLikeBoth;
     }
 
-    for (int i = peopleShow.length - 1; i >= 0; i--) {
-      bool contain = true;
-      for (int j = 0; j < _categoryFilter.length; j++) {
-        contain =
-            contain & peopleShow[i].categoryList.contains(_categoryFilter[j]);
-      }
-      if (!contain) {
-        peopleShow.removeAt(i);
-      }
-    }
+    // if (_likeFilter) {
+    //   peopleShow.clear();
+    //   for (int i = 0; i < people.length; i++) {
+    //     if (people[i].like) {
+    //       peopleShow.add(people[i]);
+    //     }
+    //   }
+    // } else {
+    //   peopleShow = List.from(people);
+    // }
+
+    // for (int i = peopleShow.length - 1; i >= 0; i--) {
+    //   bool contain = true;
+    //   for (int j = 0; j < _categoryFilter.length; j++) {
+    //     contain =
+    //         contain & peopleShow[i].categoryList.contains(_categoryFilter[j]);
+    //   }
+    //   if (!contain) {
+    //     peopleShow.removeAt(i);
+    //   }
+    // }
+
     return CupertinoPageScaffold(
       child: SafeArea(
         child: Column(
@@ -170,7 +402,7 @@ class _LikeListPageState extends State<LikeListPage> {
                   Expanded(
                     flex: 1,
                     child: CupertinoButton(
-                      child: Text(_likeFilter ? "like" : "전체"),
+                      child: Text(_likeFilterText[_likeFilter]),
                       onPressed: () => _showLike(context),
                     ),
                   ),
@@ -200,7 +432,7 @@ class _LikeListPageState extends State<LikeListPage> {
                           // ),
                           leading: ClipRRect(
                             borderRadius: BorderRadius.circular(15),
-                            child: peopleShow[index].profileImage,
+                            child: Image.asset(peopleShow[index].profileImage),
                           ),
                         ));
                   }),
