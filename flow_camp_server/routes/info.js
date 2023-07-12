d32fa882d016468005ec0d58028939c1cf9f995f
skykhs3 <skykhs3@naver.com>
2023-07-12 13:30:22 +0900

    Feat(back/info.js) info 라우터에 흥미, 유저 정보 수정하는 로직 추가

--- flow_camp_server/routes/info.js
+++ flow_camp_server/routes/info.js
@@ -1,16 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const {User} = require('../models');
 
 /* GET home page. */
 router.patch('/', async function (req, res, next) {
+    var myID = 2;
     try {
         console.log("info patch!!");
-        const {name, gradOf, universityId, prtcpntYear, interest } = req.body;
+        const { name, gradOf, universityId, prtcpntYear, interest } = req.body; // assuming 'interest' is an array of interest ids
         console.log(name, gradOf, universityId, prtcpntYear, interest);
+
+        // find the user first
+        const user = await User.findByPk(myID);
+
+        if (user) {
+            // update the user
+            await user.update({
+                name: name,
+                gradOf: gradOf,
+                universityId: universityId,
+                prtcpntYear: prtcpntYear,
+                infoConfirmed: true,
+            });
+
+            // update interests
+            await user.setInterests(interest); // this method is provided by Sequelize for many-to-many relationships, it will replace old interests with the new ones
+
+            res.status(200).send('User updated successfully');
+        } else {
+            res.status(404).send('User not found');
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
+
 
 module.exports = router;
